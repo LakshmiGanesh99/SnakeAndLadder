99a1ebab64c15caeb7d2a99bfccb08c9ebb1fb46
LakshmiGanesh99 <lakshmiganeshk99@gmail.com>
2020-09-28 00:24:53 +0530

    uc1 commit

--- snake.java
+++ snake.java
@@ -1,5 +1,6 @@
 public class snake{
 	public static void main(String[] args) {
-				System.out.println("Welcome to snake and lader game");
+		int position = 0;
+		System.out.println("Players current position is :" + position);
 	}
 }
