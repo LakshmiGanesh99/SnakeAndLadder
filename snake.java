d765a3d2c066986e44541cc546fb3c229f4ada0f
LakshmiGanesh99 <lakshmiganeshk99@gmail.com>
2020-09-28 00:40:36 +0530

    uc2 commit

--- snake.java
+++ snake.java
@@ -2,5 +2,8 @@ public class snake{
 	public static void main(String[] args) {
 		int position = 0;
 		System.out.println("Players current position is :" + position);
+		
+		int dieNumber = ((int) Math.floor(Math.random()*10) % 6) + 1;
+		System.out.println("The number rolled on die is " + dieNumber);
 	}
 }
